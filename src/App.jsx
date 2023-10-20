46c8f72f63294194f68b0b9a09320f71f4d3ee9f
Troy <TroyWarez@troysdomain.com>
2023-10-19 18:50:48 -0700

    Small changes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import darkModeIcon from '/dark_mode.svg'
+import lightModeIcon from '/light_mode.svg'
+
 import './App.css'
 
 function App() {
@@ -10,10 +13,10 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={darkModeIcon} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={lightModeIcon} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
