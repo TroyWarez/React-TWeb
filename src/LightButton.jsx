4d7a6b63e8e0e085bf65ac1617b19c6411d32bb4
Troy <TroyWarez@troysdomain.com>
2023-11-01 21:15:00 -0700

    Works with local storage

--- src/LightButton.jsx
+++ src/LightButton.jsx
@@ -4,24 +4,52 @@ import darkModeIcon from '/dark_mode.svg'
 import './LightButton.css'
 function LightButton()
 {
-    let Icon = null;
-    switch(getComputedStyle(document.body).getPropertyValue('--main-bg-color'))
-    {
-        case '#131313':
+    let Iconsrc = null;
+    let lightSwitchState = null;
+    if (typeof(Storage) !== "undefined") {
+       lightSwitchState = localStorage.getItem('lightSwitchState');
+       if(lightSwitchState === null)
+       {
+            localStorage.setItem('lightSwitchState', 'dark');
+            lightSwitchState = 'dark';
+       }
+        switch (lightSwitchState)
+        {
+            case 'dark':
             {
-                Icon = lightModeIcon;
+                document.body.style.setProperty('--main-bg-color', '#131313');
+                Iconsrc = lightModeIcon;
                 document.body.style.setProperty('--main-bg-hovercolor', '#393939');
                 break;
             }
-        case '#ffffff':
+            case 'light':
             {
-                Icon = darkModeIcon;
+                document.body.style.setProperty('--main-bg-color', '#ffffff');
+                Iconsrc = darkModeIcon;
                 document.body.style.setProperty('--main-bg-hovercolor', '#e2e2e2');
                 break;
             }
-                
+        }
+        return (<input className='lightSwitch' type='image' src={Iconsrc} onClick={switchLightMode}/>);
     }
-    return (<input className='lightSwitch' type='image' src={Icon} onClick={switchLightMode}/>);
+        switch (getComputedStyle(document.body).getPropertyValue('--main-bg-color'))
+        {
+            case '#131313':
+            {
+                document.body.style.setProperty('--main-bg-color', '#131313');
+                Iconsrc = lightModeIcon;
+                document.body.style.setProperty('--main-bg-hovercolor', '#393939');
+                break;
+            }
+            case '#ffffff':
+            {
+                document.body.style.setProperty('--main-bg-color', '#ffffff');
+                Iconsrc = darkModeIcon;
+                document.body.style.setProperty('--main-bg-hovercolor', '#e2e2e2');
+                break;
+            }    
+        }
+    return (<input className='lightSwitch' type='image' src={Iconsrc} onClick={switchLightMode}/>);
 }
 function switchLightMode(event)
 {
@@ -36,6 +64,7 @@ function switchLightMode(event)
               {
                 document.body.style.setProperty('--main-bg-color', '#ffffff');
                 document.body.style.setProperty('--main-bg-hovercolor', '#e2e2e2');
+                localStorage.setItem('lightSwitchState', 'light');
                 InputElement.src = darkModeIcon;
                 break;
               }
@@ -43,6 +72,7 @@ function switchLightMode(event)
               {
                 document.body.style.setProperty('--main-bg-color', '#131313');
                 document.body.style.setProperty('--main-bg-hovercolor', '#393939');
+                localStorage.setItem('lightSwitchState', 'dark');
                 InputElement.src = lightModeIcon;
                 break;
               }
