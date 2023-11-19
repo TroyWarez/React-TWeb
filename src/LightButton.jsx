978b5362bd45979c3695dc2d1fd50e36a5e42909
Troy <TroyWarez@troysdomain.com>
2023-11-18 17:52:50 -0700

    refactor

--- src/LightButton.jsx
+++ src/LightButton.jsx
@@ -3,65 +3,74 @@ import darkModeIcon from '/dark_mode.svg'
 import terminalDarkIcon from '/terminal_dark.svg'
 import terminalLightIcon from '/terminal_light.svg'
 import './LightButton.css'
-function LightButton(name, key)
-{
-    let Iconsrc = null;
-    let lightSwitchState = null;
-    switch (getComputedStyle(document.body).getPropertyValue('--main-bg-color'))
-    {
-        case '#131313':
-        {
-            lightSwitchState = 'dark';
-            break;
-        }
-        case '#ffffff':
-        {
-            lightSwitchState = 'light';
-            break;
-        }
-        default:
-        {
-            lightSwitchState = 'dark';
-            console.warn('Warning: \'--main-bg-color\' was not set correctly in localStorage');
-            break;
-        }
-    }
-    if (typeof(Storage) !== "undefined") {
-       lightSwitchState = localStorage.getItem('lightSwitchState');
-       if(lightSwitchState === null)
-       {
-            localStorage.setItem('lightSwitchState', lightSwitchState);
-       }
-    }
 
-    switch (lightSwitchState)
+// localStorage is where we store the users perfrences and we
+class LightModeHandler
+{
+    constructor(name, key)
+    {
+        this.blocalStorage = false;
+        this.Iconsrc = null;
+        let lightSwitchState = null;
+        switch (getComputedStyle(document.body).getPropertyValue('--main-bg-color'))
         {
-            case 'dark':
+            case '#131313':
             {
-                document.body.style.setProperty('--main-bg-color', '#131313');
-                Iconsrc = lightModeIcon;
-                document.body.style.setProperty('--main-bg-hovercolor', '#393939');
-                document.body.style.setProperty('--main-hovertextColor', '#d3d3d3');
-                document.body.style.setProperty('--main-scheme', 'dark');
-                document.body.style.setProperty('--main-textColor', '#ffffff');
+                lightSwitchState = 'dark';
                 break;
             }
-            case 'light':
+            case '#ffffff':
             {
-                document.body.style.setProperty('--main-bg-color', '#ffffff');
-                Iconsrc = darkModeIcon;
-                document.body.style.setProperty('--main-bg-hovercolor', '#e2e2e2');
-                document.body.style.setProperty('--main-hovertextColor', '#c5c5c5');
-                document.body.style.setProperty('--main-scheme', 'light');
-                document.body.style.setProperty('--main-textColor', '#131313');
+                lightSwitchState = 'light';
+                break;
+            }
+            default:
+            {
+                lightSwitchState = 'dark';
+                console.warn('Warning: \'--main-bg-color\' was not set correctly in localStorage');
                 break;
             }
         }
-    return (<input className='lightSwitch' type='image' src={Iconsrc} onClick={switchLightMode} alt={lightSwitchState} value={name} key={key}/>);
-}
-function switchLightMode(event)
-{
-    let terminalIcon = document.getElementsByClassName('terminalIcon')[0];
+        if (typeof(Storage) !== "undefined") {
+        this.blocalStorage = true;
+        lightSwitchState = localStorage.getItem('lightSwitchState');
+        if(lightSwitchState === null)
+           {
+                localStorage.setItem('lightSwitchState', lightSwitchState);
+           }
+        }
+    
+        switch (lightSwitchState)
+            {
+                case 'dark':
+                {
+                    document.body.style.setProperty('--main-bg-color', '#131313');
+                    this.Iconsrc = lightModeIcon;
+                    document.body.style.setProperty('--main-bg-hovercolor', '#393939');
+                    document.body.style.setProperty('--main-hovertextColor', '#d3d3d3');
+                    document.body.style.setProperty('--main-scheme', 'dark');
+                    document.body.style.setProperty('--main-textColor', '#ffffff');
+                    break;
+                }
+                case 'light':
+                {
+                    document.body.style.setProperty('--main-bg-color', '#ffffff');
+                    this.Iconsrc = darkModeIcon;
+                    document.body.style.setProperty('--main-bg-hovercolor', '#e2e2e2');
+                    document.body.style.setProperty('--main-hovertextColor', '#c5c5c5');
+                    document.body.style.setProperty('--main-scheme', 'light');
+                    document.body.style.setProperty('--main-textColor', '#131313');
+                    break;
+                }
+            }
+            this.lightButton = <input className='lightSwitch' id='lightSwitch' type='image' src={this.Iconsrc} onClick={this.switchLightMode.bind(this)} alt={lightSwitchState} value={name} key={key}/>
+    }
+    getLightButton()
+    {
+        return this.lightButton;
+    }
+    switchLightMode(event)
+    {
     switch (getComputedStyle(document.body).getPropertyValue('--main-bg-color'))
       {
           case '#131313':
@@ -69,15 +78,15 @@ function switchLightMode(event)
                 document.body.style.setProperty('--main-bg-color', '#ffffff');
                 document.body.style.setProperty('--main-bg-hovercolor', '#e2e2e2');
                 document.body.style.setProperty('--main-hovertextColor', '#c5c5c5');
-                localStorage.setItem('lightSwitchState', 'light');
+                if(this.blocalStorage === true)
+                {
+                    localStorage.setItem('lightSwitchState', 'light');
+                }
                 event.target.src = darkModeIcon;
-                event.target.alt = 'light';
+                event.target.alt = 'Light';
                 document.body.style.setProperty('--main-scheme', 'light');
                 document.body.style.setProperty('--main-textColor', '#131313');
-                if( (typeof terminalIcon  !== undefined) )
-                {
-                    terminalIcon.src = terminalLightIcon;
-                }
+                this.lightButton = terminalLightIcon;
                 break;
               }
           case '#ffffff':
@@ -85,18 +94,29 @@ function switchLightMode(event)
                 document.body.style.setProperty('--main-bg-color', '#131313');
                 document.body.style.setProperty('--main-bg-hovercolor', '#393939');
                 document.body.style.setProperty('--main-bg-hovertextColor', '#c5c5c5');
-                localStorage.setItem('lightSwitchState', 'dark');
-                event.target.alt = 'dark';
+                if(this.blocalStorage === true)
+                {
+                    localStorage.setItem('lightSwitchState', 'dark');
+                }
+                event.target.alt = 'Dark';
                 event.target.src = lightModeIcon;
                 document.body.style.setProperty('--main-scheme', 'dark');
                 document.body.style.setProperty('--main-textColor', '#ffffff');
-                if( (typeof terminalIcon  !== undefined) )
-                {
-                    terminalIcon.src = terminalDarkIcon;
-                }
+                this.lightButton = terminalDarkIcon;
                 break;
               }
                   
       }
+    }
 }
+let __LightButton = null;
+function LightButton(name, key)
+{
+    if(__LightButton === null)
+    {
+        __LightButton = new LightModeHandler(name, key); 
+    }
+    return __LightButton.getLightButton();
+}
+
 export default LightButton
